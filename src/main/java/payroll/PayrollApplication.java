d02bf30dddde004bb9165d039421e1a02842e16f
Luke Clover <lukphicl@umich.edu>
2021-04-14 00:05:50 -0400

    Implemented HATEOAS

--- src/main/java/payroll/PayrollApplication.java
+++ src/main/java/payroll/PayrollApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PayrollApplication {
 
+
+	// Left off - What makes Something RESTful? -> Getting an aggregate roor
+
 	public static void main(String[] args) {
 		SpringApplication.run(PayrollApplication.class, args);
 	}
